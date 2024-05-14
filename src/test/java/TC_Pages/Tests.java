dc8d3cb04c9115f6f10f542b114f4e43631af2d7
hossa <hossam.essam@our-edu.net>
2024-05-14 21:06:49 +0300

    change

--- src/test/java/TC_Pages/Tests.java
+++ src/test/java/TC_Pages/Tests.java
@@ -1,16 +1,19 @@
 package TC_Pages;
 
+import Pages.SignUp.SignUpScenarios;
 import TC_Pages.TC_SignUp.TC_SignUpPage;
 import org.json.simple.parser.ParseException;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 public class Tests {
+    WebDriver driver;
     @Test
     public void teest() throws IOException, ParseException {
-        new TC_SignUpPage().createAccountWithValidData();
+        new SignUpScenarios(driver).verifyValidScenarioWithValidData();
     }
 
 }
