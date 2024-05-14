08a665fc3efd7ca8e44a4b85bdea863f5e2302a8
hossa <hossam.essam@our-edu.net>
2024-05-14 20:30:32 +0300

    change

--- src/test/java/TC_Pages/Tests.java
+++ src/test/java/TC_Pages/Tests.java
@@ -1,10 +1,22 @@
 package TC_Pages;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Tests {
     @Test
     public void teest(){
         System.out.println("teest");
+    }
+
+    @Test
+    public void teest1(){
+        System.out.println("teest");
+    }
+
+    @Test
+    public void teest2(){
+        System.out.println("teest");
+        Assert.fail();
     }
 }
