aeef6f923f0b21fe412e2e07f7f7235eace741e9
hossa <hossam.essam@our-edu.net>
2024-05-14 21:04:18 +0300

    change

--- src/test/java/TC_Pages/Tests.java
+++ src/test/java/TC_Pages/Tests.java
@@ -1,23 +1,16 @@
 package TC_Pages;
 
+import TC_Pages.TC_SignUp.TC_SignUpPage;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class Tests {
     @Test
-    public void teest(){
-        System.out.println("teest");
+    public void teest() throws IOException, ParseException {
+        new TC_SignUpPage().createAccountWithValidData();
     }
 
-    @Test
-    public void test(){
-        System.out.println("teest");
-    }
-
-    @Test
-    public void teestt(){
-        System.out.println("teest");
-
-
-    }
 }
