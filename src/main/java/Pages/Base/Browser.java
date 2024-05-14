0968dc19b9d0af4207419f6658a9c2bde99265c0
hossa <hossam.essam@our-edu.net>
2024-05-14 16:57:31 +0300

    test

--- src/main/java/Pages/Base/Browser.java
+++ src/main/java/Pages/Base/Browser.java
@@ -2,13 +2,15 @@ package Pages.Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Browser {
     private static WebDriver driver ;
     private static String url = "https://demo.nopcommerce.com/";
-    public static WebDriver setBrowser()
-    {
-        driver = new ChromeDriver();
+    public static WebDriver setBrowser() {
+        ChromeOptions opt = new ChromeOptions();
+        opt.addArguments("--headless=new");
+        driver = new ChromeDriver(opt);
         driver.manage().window().maximize();
         driver.get(url);
         return Browser.driver;
